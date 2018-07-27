7273ab7bcab215d9efeb3e19e7118f7d52bdd4da
Larry Buffaloboy <larrybuffaloboy@gmail.com>
2018-07-27 18:03:03 -0400

    Added To do List page and schema

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ var db = mongoose.connection;
 //handle mongo error
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function () {
-  // we're connected!
+  console.log('we are connected!');
 });
 
 
